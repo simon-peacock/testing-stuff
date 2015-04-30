bdaf21f641b15a65d0578b7366d687e5ef589644
root <root@packer-debian-7.4-amd64>
2015-04-30 15:26:04 +0000

    committing rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-            if [ rsync -avh --progress -e "ssh" /var/lib/jenkins/jobs/test-ubm/workspace/docroot jenkins@178.62.121.220:/var/www ]; then
-               echo "we rsynced"
-
-#rsync -avh --progress -e "ssh" /var/lib/jenkins/jobs/test-ubm/workspace/docroot jenkins@178.62.121.220:/var/www
-
-            else
-               echo "well that didn't work rsyncing"
-            fi
+if [ rsync -avh --progress -e "ssh" /var/lib/jenkins/jobs/test-ubm/workspace/docroot jenkins@178.62.121.220:/var/www ]; then
+echo "we rsynced"
+else
+echo "well that didn't work rsyncing"
+fi
 
 
