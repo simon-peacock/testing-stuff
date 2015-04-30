87ee3cb22c44a566f6546a54d29d7ae876344adf
root <root@packer-debian-7.4-amd64>
2015-04-30 10:16:36 +0000

    Changing rsync file

--- rsync.sh
+++ rsync.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+            if [ sudo cp -R "/var/lib/jenkins/jobs/test-ubm/workspace/docroot" "/var/www/docroot" ]; then
+               echo "it moved to the docroot"
+            else
+               echo "The file failed to move to the docroot"
 
-sudo cp -R "/var/lib/jenkins/jobs/test-ubm/workspace/docroot" "/var/www/docroot"
+
+            if [ sudo cp -R "/var/lib/jenkins/jobs/test-ubm/workspace/docroot" "/var/www/html" ]; then
+               echo "it moved to the html ok"
+            else
+               echo "The file failed to move to the html"
+
+            fi
+
+
