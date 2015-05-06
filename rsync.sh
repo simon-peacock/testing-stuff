6ce827704e3120e577b8b7ebe9fe3951f069271c
simon-peacock <si@simonpeacock.co.uk>
2015-05-06 14:36:20 +0100

    Update rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
- rsync -avh --progress --delete --exclude /sites/default/settings.php /sites/default/foobar -e  "ssh" /var/lib/jenkins/jobs/UBM_enso/workspace/docroot/ jenkins@gbuweb04-int.aws.ubm-net.com:/home/jenkins/docroot/
+ rsync -avrh --progress --delete --exclude-from exlude_deletions.txt -e  "ssh" /var/lib/jenkins/jobs/UBM_enso/workspace/docroot/ jenkins@gbuweb04-int.aws.ubm-net.com:/home/jenkins/docroot/
